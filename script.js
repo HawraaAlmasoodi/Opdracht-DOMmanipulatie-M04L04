ab12c7c56123b07e05e9ea5726a67e50e871c3c3
Hawraa Al Masoodi <hawraaalmasoodi@MacBook-Air-van-Hawraa.local>
2020-08-25 10:39:51 +0200

    DOM Manipulatie

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// Deel 1
+let btns = document.querySelectorAll(".big-five-button");
+Array.from(btns).forEach(btn => {
+    btn.addEventListener('click', function () {
+        const spottedAnimal = btn.innerHTML;
+        let newListItem = document.createElement("li");
+        let newListItemValue = document.createTextNode(spottedAnimal);
+        newListItem.appendChild(newListItemValue);
+        let spottedList = document.getElementById("spotted-animals-list");
+        spottedList.appendChild(newListItem);
+
+    });
+});
+
+
+// Deel 2
+
+let removeFirstItem = () => {
+    let parent = document.getElementById("spotted-animals-list");
+    //let child = document.getElementsByClassName("spotted-animals-list-item");
+    parent.removeChild(parent.firstElementChild);
+}
+
+let removeListItemButton = document.getElementById("remove-first-item-button");
+removeListItemButton.addEventListener("click", removeFirstItem);
+
+// Deel 3
+
+let removeAllItems = () => {
+    const spottedAnimalsList = document.getElementById("spotted-animals-list");
+    spottedAnimalsList.innerHTML = "";
+}
+
+let removeAllButtons = document.getElementById("remove-all-button");
+removeAllButtons.addEventListener("click", removeAllItems);
+
+
